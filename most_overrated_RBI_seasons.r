44537204b947d6f200614c8439264e3c6b9b88ee
Steve Lyster <steven.lyster@gmail.com>
2019-09-06 20:56:24 -0600

    Add files via upload

--- most_overrated_RBI_seasons.r
+++ most_overrated_RBI_seasons.r
@@ -1,10 +1,37 @@
 library("readr")
 library("dplyr")
 
-setwd("D:/Data")
+#setwd("D:/Data")
 
 #Load in the data
-dat <- read_csv("FG_Dashboard_1946-2019_Offense.csv")
+#dat <- read_csv("FG_Dashboard_1946-2019_Offense.csv")
+urlfile <- "https://raw.githubusercontent.com/vaguely-right/Baseball/master/FG_Dashboard_1946-2019_Offense.csv?token=AMYNFAACD7NSCH3QC4YOKBK5OB4GC"
+dat <- read_csv(urlfile)
+
+#Scrape the data directly from the website
+library("textreadr")
+library("rvest")
+library("tibble")
+url <- "https://www.fangraphs.com/leaders.aspx?pos=all&stats=bat&lg=all&qual=y&type=c,4,6,11,12,13,21,-1,34,35,40,41,-1,23,37,38,50,61,52,51,53,-1,111,-1,203,199,58&season=2019&month=0&season1=1946&ind=1&team=&rost=&age=&filter=&players=&startdate=&enddate=&page=1_10000"
+xp <- '//*[@id="LeaderBoard1_dg1_ctl00"]'
+
+dat <- url %>%
+  read_html() %>%
+  html_nodes(xpath=xp) %>%
+  html_table()
+
+dat1 <- dat[[1]]
+dat1 <- dat1[-c(1,3),]
+colnames(dat1) <- dat1[1,]
+dat1 <- dat1[-c(1),]
+View(dat1)
+
+dat <- as_tibble(dat1)
+dat$RBI <- as.numeric(dat$RBI)
+dat$wRC <- as.numeric(dat$wRC)
+dat$wRAA <- as.numeric(dat$wRAA)
+dat$BsR <- as.numeric(dat$BsR)
+dat$Bat <- as.numeric(dat$Bat)
 
 #Get the columns that we want
 colnames(dat)
