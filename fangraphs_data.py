5f36fc3bd63b70c21ba5c3138c422540d94b340f
Steve Lyster <steven.lyster@gmail.com>
2020-01-27 21:50:05 -0700

    Update fangraphs_data.py

--- fangraphs_data.py
+++ fangraphs_data.py
@@ -70,15 +70,25 @@ df[['PA','xPA']]
 url = 'https://www.fangraphs.com/leaders.aspx?pos=all&stats=bat&lg=all&qual=0&type=c,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,37,38,41,35,34&season=2019&month=0&season1=1955&ind=0&team=0,ss&rost=0&age=0&filter=&players=0&startdate=1955-01-01&enddate=2019-12-31'
 df = query_fangraphs(url)
 
+#%% Should have left the glove at home
+url = 'https://www.fangraphs.com/leaders.aspx?pos=all&stats=bat&lg=all&qual=y&type=8&season=2019&month=0&season1=1973&ind=1&team=&rost=&age=&filter=&players=&startdate=&enddate='
+df = query_fangraphs(url)
+df = df[["Name","Season", "G", "wRC+", "Def", "WAR"]]
+df['DefDH'] = -17.5 * df.G / 162
+df['DefImp'] = df.DefDH - df.Def
+df['DefImp162'] = df.DefImp * 162 / df.G
+df.sort_values(by='DefImp',ascending=False).head(25)
+df.sort_values(by='DefImp162',ascending=False).head(25)
 
-
-
-
-
-
-
-
-
+#%% Career numbers
+url = 'https://www.fangraphs.com/leaders.aspx?pos=all&stats=bat&lg=all&qual=y&type=8&season=2019&month=0&season1=1973&ind=0&team=&rost=&age=&filter=&players=&startdate=&enddate='
+df = query_fangraphs(url)
+df = df[["Name", "G", "wRC+", "Def", "WAR"]]
+df['DefDH'] = -17.5 * df.G / 162
+df['DefImp'] = df.DefDH - df.Def
+df['DefImp162'] = df.DefImp * 162 / df.G
+df.sort_values(by='DefImp',ascending=False).head(25)
+df.sort_values(by='DefImp162',ascending=False).head(25)
 
 
 
